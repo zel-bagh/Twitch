93a665aef4db9bd05b34b84637aa1388ae4b3294
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2024-10-31 18:05:20 +0100

    Task pending: understading how dark mode is done in general, then implement the dark mode behavior with shadcn help

--- twitch/app/(auth)/_components/logo.tsx
+++ twitch/app/(auth)/_components/logo.tsx
@@ -8,9 +8,20 @@ const font = Poppins({
 })
 
 export const Logo = () => {
+     /* can be done using cn() */
     return (
-        <div>
-            Logo!
+        <div className="flex flex-col items-center gap-2">
+            <div className="bg-white rounded-full">
+                <Image
+                    src="/spooky.svg"
+                    alt="Twitch"
+                    width={80}
+                    height={80} />
+            </div>
+            <div className={"flex flex-col items-center " + font.className}>
+                <p className="text-xl font-semibold"> Twitch </p>
+                <p className="text-sm text-muted-foreground"> Let's Play </p>
+            </div>
         </div>
     );
 };
--- twitch/app/(auth)/layout.tsx
+++ twitch/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./_components/logo"
 
 export default function AuthLayout ({ children } : { children : React.ReactNode}) {
 return (
-    <div className="h-full flex flex-col items-center justify-center">
+    <div className="h-full flex flex-col items-center justify-center gap-4">
         <Logo />
         {children}
     </div>
